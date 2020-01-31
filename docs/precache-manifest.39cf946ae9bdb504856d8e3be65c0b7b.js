0342e35225697fa3e986d97903ac4f1e9e2451f4
Johan <johan@macair2020>
2020-01-31 04:44:47 +0100

    new GH Pages version

---
+++ docs/precache-manifest.39cf946ae9bdb504856d8e3be65c0b7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "038842f3dc462eaac16ea7f1a51a40d2",
+    "url": "/js-finance/index.html"
+  },
+  {
+    "revision": "fcc59563d64c5ff85a81",
+    "url": "/js-finance/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "fcc59563d64c5ff85a81",
+    "url": "/js-finance/static/js/2.453acaef.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/js-finance/static/js/2.453acaef.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f0d2ff1c648737853ef0",
+    "url": "/js-finance/static/js/main.88081298.chunk.js"
+  },
+  {
+    "revision": "7974c1eef9aea19dd308",
+    "url": "/js-finance/static/js/runtime-main.76eb48b7.js"
+  }
+]);
